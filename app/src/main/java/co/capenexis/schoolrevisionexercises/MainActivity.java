68b309ed027e3d2b2f4bd792f9c2a006539943ca
Wisdom Hove <62532064+WisdomHove@users.noreply.github.com>
2020-09-13 15:08:39 +0200

    Initial commit

---
+++ app/src/main/java/co/capenexis/schoolrevisionexercises/MainActivity.java
@@ -0,0 +1,33 @@
+package co.capenexis.schoolrevisionexercises;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.web_view);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl(("file:///android_asset/index.html"));
+        webView.setWebViewClient(new WebViewClient());
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
